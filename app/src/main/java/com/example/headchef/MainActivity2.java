9012bfcd056332b9ad6c2cb3cf082c58552fc31c
TEJAS-252000 <tejasnazre@gmail.com>
2021-06-23 14:14:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/headchef/MainActivity2.java
@@ -0,0 +1,29 @@
+package com.example.headchef;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main2);
+        getSupportActionBar().hide();
+        WebView webView = findViewById(R.id.webview);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://pes-restaurant.herokuapp.com/km.php");
+        webView.setWebViewClient(new WebViewClient());
+    }
+}
